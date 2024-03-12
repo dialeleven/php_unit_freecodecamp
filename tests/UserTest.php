0499664a1196586b3c55953729c14b6da72bf91c
Francis Tsao <dialeleven@gmail.com>
2024-03-11 22:59:35 -0400

    Fixed src filename (UserClass.php to User.php) and namespace not matching what is in composer.json

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -1,10 +1,17 @@
 <?php
-// https://www.freecodecamp.org/news/test-php-code-with-phpunit/
-namespace Zubair\TestProject;
+namespace Francis\PhpUnitFreecodecamp;
 
 use PHPUnit\Framework\TestCase;
 
 final class UserTest extends TestCase
 {
     // Tests will go here
+    public function testClassConstructor()
+    {
+      $user = new User(18, 'John');
+
+      $this->assertSame('John', $user->name);
+      $this->assertSame(18, $user->age);
+      $this->assertEmpty($user->favorite_movies);
+    }
 }

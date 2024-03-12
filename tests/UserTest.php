58dc74762118f4f81b8244aa52e28620e63cdd53
Francis Tsao <dialeleven@gmail.com>
2024-03-11 22:45:29 -0400

    UserTest class

---
+++ tests/UserTest.php
@@ -0,0 +1,10 @@
+<?php
+// https://www.freecodecamp.org/news/test-php-code-with-phpunit/
+namespace Zubair\TestProject;
+
+use PHPUnit\Framework\TestCase;
+
+final class UserTest extends TestCase
+{
+    // Tests will go here
+}
